7c39b16dab78bdf7b98e77165e9030ced6464923
Raghad Butaiban <raghadbutaiban@gmail.com>
2025-04-06 11:36:46 +0300

    Components done

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent, ContentComponent, HomeComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
